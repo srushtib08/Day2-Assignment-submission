9343b1cbe1d7af7795245ae0a0387ff47da251a7
Srushti Badukale <89703584+srushtib08@users.noreply.github.com>
2022-02-27 20:44:20 +0530

    Assignment submitted successfully!

---
+++ Day2/Assignment1.js
@@ -0,0 +1,82 @@
+// Arithmetic operators-->+,-,*,/,%
+var num1=10,num2=9;
+res=num1+num2;
+console.log(res);
+
+res=num1-num2;
+console.log(res);
+
+res=num1*num2;
+console.log(res);
+
+res=num1/num2;
+console.log(res);
+
+res=num1%num2;
+console.log(res); 
+
+//Assignment operators--> =,+=,-+,*=,/=,%=
+// res=num1=6;
+// console.log(res);
+
+// res=num1+=5;
+// console.log(res);
+
+// res=num1-=5;
+// console.log(res);
+
+// res=num1*=5;
+// console.log(res);
+
+// res=num1/=5;
+// console.log(res);
+
+// res=num1%=5;
+// console.log(res);
+
+//comparison operators/relational operators--> ==,!=,>,<,>=,<=
+// var num1=7, num2=7;
+// res=num1==num2;
+// console.log(res);
+
+// res=num1!=num2;
+// console.log(res);
+
+// res=num1>num2;
+// console.log(res);
+
+// res=num1<num2;
+// console.log(res);
+
+// res=num1>=num2;
+// console.log(res);
+
+// res=num1<=num2;
+// console.log(res);
+
+//identity operators--> ===,!==
+// var num1=4, num2=3;
+// res=num1===num2;
+// console.log(res);
+
+// res=num1!==num2;
+// console.log(res);
+
+//conditional operators/logical operator--> &&,||,!
+// var num1=7, num2=8;
+// res=num1&&num2;
+// console.log(res);
+
+// res=num1||num2;
+// console.log(res);
+
+// res=!(num1&&num2);
+// console.log(res);
+
+//unary operators--> ++,--
+// var num1=8;
+// num=num1++;
+// console.log(num);
+
+// num=num1--;
+// console.log(num);
